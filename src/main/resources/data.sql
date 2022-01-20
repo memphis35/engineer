73d600ceb5d5c4cc719b2053817fffd68cedfb57
Smirnov A <aa.smirnow@outlook.com>
2022-01-20 22:48:04 +0300

    fix data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,16 +10,16 @@ values (1, 'Конструкторский центр'),
        (5, 'УОЗ')
 on conflict do nothing;
 
-insert into engineer.public.users (id, email, password, name, department_id, role, expiration_date)
+insert into engineer.public.users (id, email, password, name, department_id, role, expiration_date, created)
 values (-1, 'asmirnov@engineer.com', '$2a$12$Poyi86C1NuAop4hu4KtZr.IXW.IhYjIWlmu.Y/Ych3Ay9T9deB/yS', 'Александр Смирнов',
         4,
-        'ROLE_ADMIN', '2022-12-31 23:59:59')
+        'ROLE_ADMIN', '2022-12-31 23:59:59', now())
 on conflict do nothing;
 
 insert into engineer.public.task (id, registration_number, title, customer_id, comment)
-values (1, 5, 'Реконструкция верхних ножниц АПР-4', 1, null),
-       (2, 7, 'Замена гидропривода ленточного захлестывателя', 1, null),
-       (3, 10, 'Ремонт хранилища маточного раствора', 1, null),
-       (4, 15, 'Реконструкция воронки центрального питателя, АГЦ-1', 1, null),
-       (5, 27, 'Расширение конвейера №3, АГЦ-2', 1, null)
+values (1, 5, 'Реконструкция верхних ножниц АПР-4', -1, null),
+       (2, 7, 'Замена гидропривода ленточного захлестывателя', -1, null),
+       (3, 10, 'Ремонт хранилища маточного раствора', -1, null),
+       (4, 15, 'Реконструкция воронки центрального питателя, АГЦ-1', -1, null),
+       (5, 27, 'Расширение конвейера №3, АГЦ-2', -1, null)
 on conflict do nothing;
